ca5834ca9a1631ed6f8681132c25b8ce7d874795
MightySergo <loshcens@gmail.com>
2023-10-02 20:29:57 +0300

    added task twentyfive

---
+++ taskTwentyFive/taskTwentyFive.cs
@@ -0,0 +1,17 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+void PowerAB(double numberA, int numberB){
+    
+    double totalNumber = numberA;
+    for (int i = 1; i < numberB; i++) // Можно конечно воспользоваться библиотекой Math, но думается задумка этого задания не в этом
+    {
+        totalNumber*=numberA;
+    }
+    Console.WriteLine($"Your total number is {totalNumber}");
+}
+
+Console.WriteLine("Enter your number: ");
+double userN = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter numbers power: ");
+int userP = Convert.ToInt32(Console.ReadLine());
+
+PowerAB(userN, userP);
