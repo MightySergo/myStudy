c282682fd31e066d5f0c7311b6231793937c84d3
MightySergo <loshcens@gmail.com>
2023-10-02 21:16:16 +0300

    added task twentySeven

---
+++ TaskTwentySeven/taskTwentySeven.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int SumDigits (int digit){
+    
+    int summed = 0;
+    while (digit != 0)
+    {
+        int number = digit%10;
+        summed += number;
+        digit /= 10;
+    }
+    return summed;
+}
+
+Console.WriteLine ("Enter your number: ");
+Console.WriteLine($"Sum of digits: {SumDigits(Convert.ToInt32(Console.ReadLine()))}");
