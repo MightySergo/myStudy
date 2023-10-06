3008f0063d34c26ca9c0f9a57d7b60ba18f5aff8
MightySergo <loshcens@gmail.com>
2023-10-06 20:11:24 +0300

    added homework for seminar six

---
+++ TaskFourtyTree/TaskFourtyThree.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, 
+// которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+void InterceptionPoint (){
+    Console.WriteLine("Enter k1 value for f(x) function: ");
+    double k1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter b1 value for f(x) function: ");
+    double b1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter k2 value for g(x) function: ");
+    double k2 =  Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter b2 value for g(x) function: ");
+    double b2 = Convert.ToDouble(Console.ReadLine());
+    if (k1 == k2)
+    {
+        Console.WriteLine("The lines are parallel.");
+        return;
+    }
+// Set the equations equal 
+// k1 * x + b1 = k2 * x + b2
+// k1x-k2x = b2 - b1
+// x(k1-k2) = b2-b1
+// x = (b2-b1)/(k1-k2)
+
+    double x = (b2-b1)/(k1-k2);
+    Console.WriteLine($"x is equal {x:f2}");
+    double y = k1 * x + b1;
+    Console.WriteLine($"y is equal {y:f2}");
+    Console.Write($"Interception point is {x:f2},{y:f2}");    
+}
+
+InterceptionPoint();
---
+++ taskFourtyOne/TaskFourtyOne.cs
@@ -0,0 +1,26 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+
+double [] userInputArray (){
+    Console.WriteLine("Enter numbers quantity: ");
+    double [] userArray = new double [Convert.ToInt32(Console.ReadLine())];
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        Console.WriteLine($"Enter your {i+1} number: ");
+        userArray[i] = Convert.ToDouble(Console.ReadLine());
+    }
+    return userArray;
+}
+
+int countPossitiveNumbers (double [] userArray){
+    int counter = 0;
+    foreach (var item in userArray)
+    {
+        if (item > 0) counter++;
+        
+    }
+    return counter;
+}
+
+Console.Write($"Number of possitive numbers is: {countPossitiveNumbers(userInputArray())}");
